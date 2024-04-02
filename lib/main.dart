48f1d8654f2ef0cf61f9f384b87702bffcfdd8e0
Shoykat361 <shoykat15-11724@diu.edu.bd>
2024-04-02 11:02:58 +0600

    First Design

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+import 'Screens/HomePage.dart';
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+
